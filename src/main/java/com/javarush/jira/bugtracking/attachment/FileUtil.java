659585bb831363d47a6b520857215db6bbab1275
gdv_krot <gdv_krot@yahoo.com>
2024-06-14 19:10:44 +0200

    Refactor the com.javarush.jira.bugtracking.attachment.FileUtil#upload method.

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -7,10 +7,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,14 +22,18 @@ public class FileUtil {
             throw new IllegalRequestDataException("Select a file to upload.");
         }
 
-        File dir = new File(directoryPath);
-        if (dir.exists() || dir.mkdirs()) {
-            File file = new File(directoryPath + fileName);
-            try (OutputStream outStream = new FileOutputStream(file)) {
-                outStream.write(multipartFile.getBytes());
-            } catch (IOException ex) {
-                throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
-            }
+        Path dirPath = Paths.get(directoryPath);
+        try {
+            // Create directories if they don't exist
+            Files.createDirectories(dirPath);
+
+            // Create a path for the file to be saved
+            Path filePath = dirPath.resolve(fileName);
+
+            // Save the file
+            Files.write(filePath, multipartFile.getBytes());
+        } catch (IOException ex) {
+            throw new IllegalRequestDataException("Failed to upload file: " + multipartFile.getOriginalFilename(), ex);
         }
     }
 
--- src/main/java/com/javarush/jira/common/error/AppException.java
+++ src/main/java/com/javarush/jira/common/error/AppException.java
@@ -3,8 +3,11 @@ package com.javarush.jira.common.error;
 import org.springframework.lang.NonNull;
 
 public class AppException extends RuntimeException {
-
     public AppException(@NonNull String message) {
         super(message);
+    }
+
+    public AppException(@NonNull String message, Exception ex) {
+        super(message, ex);
     }
 }
